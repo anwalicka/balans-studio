7c03700d10b0381eac43fda5009c10398a73c434
unknown <anwalicka@gmail.com>
2020-06-19 19:40:39 +0200

    main page balans studio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
