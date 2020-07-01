0d1bf5fd6b76e2fd64d170f7ff89bc0d6dcb2c6c
unknown <anwalicka@gmail.com>
2020-07-01 21:39:24 +0200

    add directive ngFor

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
   @Input() styleClass: string;
+  public navList = [
+    {routerLink: '/main', name: "Strona główna" },
+    {routerLink: '/about-me', name: "O mnie" },
+    {routerLink: '', name: "Warsztaty pięknego ciała" },
+    {routerLink: '', name: "Warsztaty stania na rękach" },
+    {routerLink: '', name: "Warsztaty akrobatyki" },
+    {routerLink: '', name: "Kontakt" },
+  ]
   constructor() { }
 
   ngOnInit(): void {
